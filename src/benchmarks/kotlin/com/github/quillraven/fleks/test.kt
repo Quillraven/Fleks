357cba4e16178cf6128bdfd8d32fd7a2afd4671c
Quillraven <quillraven@gmail.com>
2021-10-20 21:36:53 +0200

    add test methods for other benchmarks

--- src/benchmarks/kotlin/com/github/quillraven/fleks/test.kt
+++ src/benchmarks/kotlin/com/github/quillraven/fleks/test.kt
@@ -1,13 +1,17 @@
 package com.github.quillraven.fleks
 
+import com.github.quillraven.fleks.collection.BitArray
+import com.github.quillraven.fleks.collection.IntBag
 import kotlin.reflect.full.createInstance
 import kotlin.system.measureNanoTime
 import kotlin.system.measureTimeMillis
 
 fun main() {
     measureReflectionCreation()
+    measureBits()
     validateFleksBenchmark()
-    compareArtemisFleks()
+    compareArtemisFleksSimple()
+    compareArtemisFleksComplex()
 }
 
 private fun measureReflectionCreation() {
@@ -22,10 +26,10 @@ private fun measureReflectionCreation() {
 
 /*
 Complex:
-Artemis: max(987)    min(865)  avg(906.3333333333334)
-Fleks:   max(1221)    min(1123)  avg(1162.6666666666667)
+Artemis: max(754)    min(688)    avg(727.6666666666666)
+Fleks:   max(949)    min(840)    avg(893.0)
  */
-private fun compareArtemisFleks() {
+private fun compareArtemisFleksComplex() {
     val artemisTimes = mutableListOf<Long>()
     val artemisState = ArtemisStateComplex().apply { setup() }
     val artemisBm = ArtemisBenchmark()
@@ -37,14 +41,85 @@ private fun compareArtemisFleks() {
     val fleksTimes = mutableListOf<Long>()
     val fleksState = FleksStateComplex().apply { setup() }
     val fleksBm = FleksBenchmark()
+    fleksBm.complex(fleksState)
     repeat(3) {
         fleksTimes.add(measureTimeMillis { fleksBm.complex(fleksState) })
     }
 
     println(
         """
+            COMPLEX:
           Artemis: max(${artemisTimes.maxOrNull()})    min(${artemisTimes.minOrNull()})  avg(${artemisTimes.average()})
           Fleks:   max(${fleksTimes.maxOrNull()})    min(${fleksTimes.minOrNull()})  avg(${fleksTimes.average()})
+      """.trimIndent()
+    )
+}
+
+/*
+Simple:
+Artemis: max(31)    min(30)  avg(30.666666666666668)
+Fleks:   max(51)    min(50)  avg(50.333333333333336)
+ */
+private fun compareArtemisFleksSimple() {
+    val artemisTimes = mutableListOf<Long>()
+    val artemisState = ArtemisStateSimple().apply { setup() }
+    val artemisBm = ArtemisBenchmark()
+    artemisBm.simple(artemisState)
+    repeat(3) {
+        artemisTimes.add(measureTimeMillis { artemisBm.simple(artemisState) })
+    }
+
+    val fleksTimes = mutableListOf<Long>()
+    val fleksState = FleksStateSimple().apply { setup() }
+    val fleksBm = FleksBenchmark()
+    fleksBm.simple(fleksState)
+    repeat(3) {
+        fleksTimes.add(measureTimeMillis { fleksBm.simple(fleksState) })
+    }
+
+    println(
+        """
+            SIMPLE:
+          Artemis: max(${artemisTimes.maxOrNull()})    min(${artemisTimes.minOrNull()})  avg(${artemisTimes.average()})
+          Fleks:   max(${fleksTimes.maxOrNull()})    min(${fleksTimes.minOrNull()})  avg(${fleksTimes.average()})
+      """.trimIndent()
+    )
+}
+
+private fun measureBits() {
+    val bitArrayTimes = mutableListOf<Long>()
+    repeat(3) {
+        bitArrayTimes.add(measureTimeMillis {
+            val bitArray = BitArray(NUM_ENTITIES)
+            val bag = IntBag()
+            repeat(NUM_ENTITIES) {
+                bitArray.set(it)
+            }
+            bitArray.toIntBag(bag)
+        })
+    }
+
+    val booleanTimes = mutableListOf<Long>()
+    repeat(3) {
+        booleanTimes.add(measureTimeMillis {
+            val bits = BooleanArray(NUM_ENTITIES)
+            val bag = IntBag()
+            repeat(NUM_ENTITIES) {
+                bits[it] = true
+            }
+            bits.forEachIndexed { idx, value ->
+                if (value) {
+                    bag.add(idx)
+                }
+            }
+        })
+    }
+
+    println(
+        """
+            BITS:
+          BitArray: max(${bitArrayTimes.maxOrNull()})    min(${bitArrayTimes.minOrNull()})  avg(${bitArrayTimes.average()})
+          Boolean[]:   max(${booleanTimes.maxOrNull()})    min(${booleanTimes.minOrNull()})  avg(${booleanTimes.average()})
       """.trimIndent()
     )
 }
