29f6af306a8dbc003c4575bacf3c0e88264ad7b0
Quillraven <quillraven@gmail.com>
2022-05-12 19:49:52 +0200

    1.1-JVM version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.quillraven.fleks"
-version = "1.0-JVM"
+version = "1.1-JVM"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val bmSourceSetName = "benchmarks"
