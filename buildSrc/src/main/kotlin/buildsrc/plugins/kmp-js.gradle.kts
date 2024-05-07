a9fa2e2839d1526880545499f7eda081281334ba
Quillraven <quillraven@gmail.com>
2024-05-07 16:43:12 +0200

    update to kotlin 1.9.24

--- buildSrc/src/main/kotlin/buildsrc/plugins/kmp-js.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/plugins/kmp-js.gradle.kts
@@ -1,7 +1,6 @@
 package buildsrc.plugins
 
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 /** conventions for a Kotlin/JS subproject */
 
@@ -15,19 +14,9 @@ kotlin {
         nodejs()
     }
 
+    @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
         nodejs()
     }
-}
-
-// https://youtrack.jetbrains.com/issue/KT-63014/
-// CompileError: WebAssembly.Module(): invalid value type 0x71
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-    download = false
-}
-tasks.withType<KotlinNpmInstallTask>().configureEach {
-    args.add("--ignore-engines")
 }
