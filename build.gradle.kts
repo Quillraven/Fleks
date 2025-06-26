c973cf8dbe18e12dff048456393e782ee02b32b0
ygdrasil-io <alexandre.mommers@gmail.com>
2025-06-27 00:50:56 +0200

    Refactor dependencies to use version catalog for coroutines-test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+                implementation(libs.coroutines.test)
             }
         }
     }
