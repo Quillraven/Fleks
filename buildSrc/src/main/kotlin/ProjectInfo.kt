453aa1f625dce9e0236cc338c4cbab8ba0f455db
Simon <quillraven@gmail.com>
2025-11-30 19:44:02 +0700

    Update project version to 2.13-SNAPSHOT

--- buildSrc/src/main/kotlin/ProjectInfo.kt
+++ buildSrc/src/main/kotlin/ProjectInfo.kt
@@ -1,5 +1,5 @@
 object ProjectInfo {
     const val GROUP = "io.github.quillraven.fleks"
-    const val VERSION = "2.12"
+    const val VERSION = "2.13-SNAPSHOT"
     const val ARTIFACT = "Fleks"
 }
